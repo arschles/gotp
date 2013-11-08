a451f719d8560a6e6d9a1366902e545717eace68
Aaron Schlesinger <arschles@gmail.com>
2013-11-07 17:09:33 -0800

    testing serialization with channels

--- test.go
+++ test.go
@@ -2,22 +2,23 @@ package main
 
 import "fmt"
 
-func work(order int, waiter chan int, next chan int) {
+func work(order int, c chan int) {
 	fmt.Printf("starting worker %d\n", order)
-	<-(waiter)
-	fmt.Printf("worker %d completed\n", order)
-	(next) <- 1
-	fmt.Printf("worker %d finished sending to next chan\n", order)
+	recvOrder := <-c
+	for recvOrder != order {
+		fmt.Printf("worker %d still waiting for its order", order)
+		recvOrder := <-c
+	}
+	fmt.Printf("worker %d completed, sending %d to channel\n", order, order+1)
+	c <- (order + 1)
 }
 
 func main() {
-	c1 := make(chan int)
-	c2 := make(chan int)
-	c3 := make(chan int)
-	c4 := make(chan int)
-	go work(0, c1, c2)
-	go work(1, c2, c3)
-	go work(2, c3, c4)
-	c1 <- 0
-	<-c4
+	c := make(chan int)
+
+	go work(0, c)
+	go work(1, c)
+	go work(2, c)
+	c <- 0
+	<-c
 }
