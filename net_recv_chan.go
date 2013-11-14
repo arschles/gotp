b0eeef55247f2da682aef7b46d2b485a951b15e3
Aaron Schlesinger <arschles@gmail.com>
2013-11-14 11:39:55 -0800

    adding a prelim version of a networked receive channel

---
+++ net_recv_chan.go
@@ -0,0 +1,68 @@
+package gotp
+
+import (
+	"encoding/gob"
+	"fmt"
+	"log"
+	"net"
+)
+
+//a type that represents a network stack that waits for messages and forwards them to a pid. all errors get forwarded to errChan
+type RecvChan struct {
+	//the Pid to foward messages to
+	forwardPid Pid
+	//the channel to send all errors to, as they happen
+	errChan chan error
+	//the host and port on which this channel listens
+	host string
+	port int
+}
+
+func (r *RecvChan) asyncSendError(err error) {
+	go func() {
+		r.errChan <- err
+	}()
+}
+
+func (r *RecvChan) NetString() string {
+	return fmt.Sprintf("%v:%v", r.host, r.port)
+}
+
+//create a new receive channel. when Start is called on the newly created channel, it will
+//listen on TCP on host:port.
+//the receiver channel will forward each message it receives on the network to the pid fp, and all errors to the channel ec
+func New(fp Pid, ec chan error, h string, p int) *RecvChan {
+	return &RecvChan{forwardPid: fp, errChan: ec, host: h, port: p}
+}
+
+type empty struct{}
+
+//start listening on the network, buffering and forwarding messages
+func (r *RecvChan) Start() {
+	starterFn := func() {
+		listener, err := net.Listen("tcp", r.NetString())
+		if err != nil {
+			log.Fatalln("error listening", err)
+			r.asyncSendError(err)
+		} else {
+			for {
+				conn, err := listener.Accept()
+				if err != nil {
+					log.Fatalln("error accepting", err)
+					r.asyncSendError(err)
+				} else {
+					decoder := gob.NewDecoder(conn)
+					decoded := empty{}
+					err := decoder.Decode(&decoded)
+					if err != nil {
+						log.Fatalln("error decoding", err)
+						r.asyncSendError(err)
+					} else {
+						r.forwardPid.Send(decoded)
+					}
+				}
+			}
+		}
+	}
+	go starterFn()
+}
