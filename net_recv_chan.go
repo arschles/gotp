3ce99088e35b63ddf5dc1009bb959625cf712e2e
Aaron Schlesinger <arschles@gmail.com>
2013-11-15 16:09:44 -0800

    converting the error channel to an error pid in net_recv_chan

--- net_recv_chan.go
+++ net_recv_chan.go
@@ -1,3 +1,6 @@
+/**
+an abstraction that runs a server on a port and forwards incoming messages to a Pid
+*/
 package gotp
 
 import (
@@ -10,8 +13,8 @@ import (
 type RecvChan struct {
 	//the Pid to foward messages to. this Pid could be a dispatcher to route messages to the right place
 	forwardPid Pid
-	//the channel to send all errors to, as they happen
-	errChan chan error
+	//the Pid to forward errors to
+	errPid Pid
 	//the channel that stops this channel from listening
 	stopChan chan struct{}
 	//the host and port on which this channel listens
@@ -19,10 +22,14 @@ type RecvChan struct {
 	port int
 }
 
-func (r *RecvChan) asyncSendError(err error) {
-	go func() {
-		r.errChan <- err
-	}()
+type NetListenError struct {
+	Err error
+}
+type NetAcceptError struct {
+	Err error
+}
+type NetDecodeError struct {
+	Err error
 }
 
 func (r *RecvChan) NetString() string {
@@ -31,16 +38,11 @@ func (r *RecvChan) NetString() string {
 
 //create a new receive channel. when Start is called on the newly created channel, it will
 //listen on TCP on host:port.
-//the receiver channel will forward each message it receives on the network to the pid fp
-func (r *RecvChan) Init(fp Pid, h string, p int) *RecvChan {
-	ec := make(chan error)
+//the receiver channel will forward each message it receives on the network to the pid fp,
+//and it will forward each error it encounters to ep
+func (r *RecvChan) Init(fp Pid, ep Pid, h string, p int) *RecvChan {
 	sc := make(chan struct{})
-	return &RecvChan{forwardPid: fp, errChan: ec, stopChan: sc, host: h, port: p}
-}
-
-//return the channel on which this receiver channel will output its errors
-func (r *RecvChan) ErrorChan() chan error {
-	return r.errChan
+	return &RecvChan{forwardPid: fp, errPid: ep, stopChan: sc, host: h, port: p}
 }
 
 //start listening on the network, buffering and forwarding messages
@@ -49,7 +51,7 @@ func (r *RecvChan) Start() {
 		listener, err := net.Listen("tcp", r.NetString())
 		if err != nil {
 			log.Fatalln("error listening", err)
-			r.asyncSendError(err)
+			r.errPid.Send(NetListenError{err})
 		} else {
 			for {
 				select {
@@ -60,14 +62,14 @@ func (r *RecvChan) Start() {
 						conn, err := listener.Accept()
 						if err != nil {
 							log.Fatalln("error accepting", err)
-							r.asyncSendError(err)
+							r.errPid.Send(NetAcceptError{err})
 						} else {
 							decoder := gob.NewDecoder(conn)
 							decoded := []Unit{}
 							err := decoder.Decode(&decoded)
 							if err != nil {
 								log.Fatalln("error decoding", err)
-								r.asyncSendError(err)
+								r.errPid.Send(NetDecodeError{err})
 							} else {
 								r.forwardPid.Send(decoded)
 							}
