1edacad7d707f93b8e01a6192eabfe1cc752d132
Aaron Schlesinger <arschles@gmail.com>
2013-11-15 17:13:02 -0800

    greatly simplifying net_recv_chan it’s no longer a struct, just a running goroutine that you can stop with a send-only channel

--- net_recv_chan.go
+++ net_recv_chan.go
@@ -1,26 +1,17 @@
 /**
-an abstraction that runs a server on a port and forwards incoming messages to a Pid
+an abstraction that runs a server on a port and forwards incoming messages to a Pid. example usage:
+	//dispatcherPid will receive messages that come over the network, and errorPid will receive all errors that occur.
+	//runs the server in the background
+	stopChan := StartRecvChan(dispatcherPid, errorPid, "localhost", 8080)
+	//do something useful
+	stopChan <- Unit{}//blocks until the server has stopped
 */
 package gotp
 
 import (
 	"encoding/gob"
-	"log"
 	"net"
 )
-
-//a type that represents a network stack that waits for messages and forwards them to a pid. all errors get forwarded to errChan
-type RecvChan struct {
-	//the Pid to foward messages to. this Pid could be a dispatcher to route messages to the right place
-	forwardPid Pid
-	//the Pid to forward errors to
-	errPid Pid
-	//the channel that stops this channel from listening
-	stopChan chan struct{}
-	//the host and port on which this channel listens
-	host string
-	port int
-}
 
 type NetListenError struct {
 	Err error
@@ -32,46 +23,34 @@ type NetDecodeError struct {
 	Err error
 }
 
-func (r *RecvChan) NetString() string {
-	return netString(r.host, r.port)
-}
-
-//create a new receive channel. when Start is called on the newly created channel, it will
-//listen on TCP on host:port.
-//the receiver channel will forward each message it receives on the network to the pid fp,
-//and it will forward each error it encounters to ep
-func (r *RecvChan) Init(fp Pid, ep Pid, h string, p int) *RecvChan {
-	sc := make(chan struct{})
-	return &RecvChan{forwardPid: fp, errPid: ep, stopChan: sc, host: h, port: p}
-}
-
-//start listening on the network, buffering and forwarding messages
-func (r *RecvChan) Start() {
+//create a new network server on host:port and start listening on the network in a goroutine.
+//all incoming messages will be forwarded to forwardPid and all errors forwarded to errPid.
+//send a Unit{} on the returned channel to stop the server. the send will block until the server is stopped
+func StartRecvChan(forwardPid Pid, errPid Pid, host string, port int) chan<- struct{} {
+	stopChan := make(chan struct{})
 	starterFn := func() {
-		listener, err := net.Listen("tcp", r.NetString())
+		listener, err := net.Listen("tcp", netString(host, port))
 		if err != nil {
-			log.Fatalln("error listening", err)
-			r.errPid.Send(NetListenError{err})
+			errPid.Send(NetListenError{err})
 		} else {
 			for {
+				defer forwardPanicToPid(errPid)
 				select {
-				case <-r.stopChan:
+				case <-stopChan:
 					break
 				default:
 					{
 						conn, err := listener.Accept()
 						if err != nil {
-							log.Fatalln("error accepting", err)
-							r.errPid.Send(NetAcceptError{err})
+							errPid.Send(NetAcceptError{err})
 						} else {
 							decoder := gob.NewDecoder(conn)
 							decoded := []Unit{}
 							err := decoder.Decode(&decoded)
 							if err != nil {
-								log.Fatalln("error decoding", err)
-								r.errPid.Send(NetDecodeError{err})
+								errPid.Send(NetDecodeError{err})
 							} else {
-								r.forwardPid.Send(decoded)
+								forwardPid.Send(decoded)
 							}
 						}
 					}
@@ -80,9 +59,5 @@ func (r *RecvChan) Start() {
 		}
 	}
 	go starterFn()
-}
-
-//stop listening on the network. blocks until the stop is complete
-func (r *RecvChan) Stop() {
-	r.stopChan <- Unit{}
+	return stopChan
 }
